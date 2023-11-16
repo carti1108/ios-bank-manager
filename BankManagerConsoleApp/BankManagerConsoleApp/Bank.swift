6c69333ffbf7fb7ad74bdb8d133b7bd859bd528e
kiseok <carti1108@naver.com>
2023-11-16 17:19:44 +0900

    docs: 콘솔 앱 파일에 Bank, BankClerk, BankManager, Customer 모델 파일 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -0,0 +1,8 @@
+//
+//  Bank.swift
+//  BankManagerConsoleApp
+//
+//  Created by Kiseok on 11/16/23.
+//
+
+import Foundation
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
@@ -0,0 +1,8 @@
+//
+//  BankManager.swift
+//  BankManagerConsoleApp
+//
+//  Created by Kiseok on 11/16/23.
+//
+
+import Foundation
