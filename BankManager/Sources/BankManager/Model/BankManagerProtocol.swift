50b4af81b50d31f0cd6f10b5000e15999ecf4e4b
kiseok <carti1108@naver.com>
2023-11-16 19:38:14 +0900

    feat: 기본 구현 추가

--- BankManager/Sources/BankManager/Model/BankManagerProtocol.swift
+++ BankManager/Sources/BankManager/Model/BankManagerProtocol.swift
@@ -1,7 +1,14 @@
 import Foundation
 
 public protocol BankManagerProtocol {
-    associatedtype T: CustomerProtocol
-    
-    func giveWaitingTicket(customerNumber: Int, customerLine: CustomerQueue<T>)
+    func giveWaitingTicket(customerNumber: Int, customerLine: CustomerQueue<Customer>)
+}
+
+extension BankManagerProtocol {
+    public func giveWaitingTicket(customerNumber: Int, customerLine: CustomerQueue<Customer>) {
+        for i in 1...customerNumber {
+           let customer = Customer(watingTicket: i)
+           customerLine.enqueue(data: customer)
+       }
+   }
 }
