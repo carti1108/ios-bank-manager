bb6b640d224ae07ec2ef2854b08df35e491caabb
kiseok <carti1108@naver.com>
2023-11-16 19:40:07 +0900

    feat: 프로토콜에서 구조체로 변경

--- BankManager/Sources/BankManager/Model/CustomerProtocol.swift
+++ BankManager/Sources/BankManager/Model/CustomerProtocol.swift
@@ -5,7 +5,10 @@
 //  Created by Kiseok on 11/15/23.
 //
 
-public protocol CustomerProtocol {
-    var waitingTicket: Int { get }
+public struct Customer{
+    public let waitingTicket: Int
+    
+    public init(watingTicket: Int) {
+        self.waitingTicket = watingTicket
+    }
 }
-
