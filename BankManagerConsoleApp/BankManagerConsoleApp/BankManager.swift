d7c42ab1013199356d08057ca09876217b237679
kiseok <carti1108@naver.com>
2023-11-16 19:41:33 +0900

    feat: BankManagerProtocol에서 기본 구현 하는 것으로 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
@@ -8,11 +8,4 @@
 import Foundation
 import BankManager
 
-struct BankManager: BankManagerProtocol {
-     func giveWaitingTicket(customerNumber: Int, customerLine: CustomerQueue<Customer>) {
-         for i in 1...customerNumber {
-            let customer = Customer(watingTicket: i)
-            customerLine.enqueue(data: customer)
-        }
-    }
-}
+struct BankManager: BankManagerProtocol { }
